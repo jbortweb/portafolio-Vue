0a536165525496085699471fc6128f2d898019cd
Jbortweb <jbortweb@gmail.com>
2022-12-20 19:04:34 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - portfoli',
