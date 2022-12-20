cd09ff979bf87fddbd8d370d8eccb3f59a6171ad
jbortweb <jbortweb@gmail.com>
2022-12-20 18:35:41 +0100

    quitar route

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,10 +4,6 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  router: {
-    base: 'portafolio-Vue'
-  },
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - portfoli',
