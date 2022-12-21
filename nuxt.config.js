68e9499b6de2d5f394beeb899a704b6b07bc1b33
Jbortweb <jbortweb@gmail.com>
2022-12-21 10:22:19 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,7 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
+      {nombre : "imagen", propiedad: "og:imagen", contenido : "fotoNombre.png"},
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
